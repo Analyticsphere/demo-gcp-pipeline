b7e5ca9748c1a9894000aa543a405488e3c95ac7
Jake Peters <jcbptrs@gmail.com>
2024-12-20 12:19:53 -0500

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+# Load base image
+FROM rocker/tidyverse:latest
+
+# Set the correct path for xelatex
+ENV PATH="$PATH:/root/bin:/usr/local/lib"
+
+# Install tinytex linux dependencies, pandoc, and rmarkdown
+# Reference: https://github.com/csdaw/rmarkdown-tinytex/blob/master/Dockerfile
+RUN apt-get update \
+  && apt-get install -y --no-install-recommends \
+    wget \
+    graphviz \ 
+    imagemagick \
+    perl && \
+    /rocker_scripts/install_pandoc.sh && \
+    install2.r rmarkdown 
+    
+# Install tinytex
+RUN Rscript -e 'tinytex::install_tinytex(repository = "illinois")'
+
+# Install R libraries
+RUN install2.r --error plumber bigrquery dplyr glue googleCloudStorageR gargle tools 
+
+# Copy R code to root directory in container
+COPY ./plumber_api.R .
+COPY ./report_iris.Rmd .
+COPY ./report_penguins.Rmd .
+COPY ./report_participant_count.Rmd .
+
+# Run R code
+ENTRYPOINT ["R", "-e","pr <- plumber::plumb('plumber_api.R'); pr$run(host='0.0.0.0', port=as.numeric(Sys.getenv('PORT')))"]
---
+++ plumber_api.R
@@ -0,0 +1,63 @@
+# Load required packages
+library(plumber)
+library(dplyr)
+library(glue)
+library(gargle)
+library(rmarkdown)
+library(googleCloudStorageR)
+library(tools)
+
+# Load environment variables from .env if it exists
+bucket <- "portal_to_box"
+
+# Define config ----------------------------------------------------------------
+report_config <- tribble(
+  ~code,  ~box_folder_id, ~rmd_name,                      ~pdf_name,
+  "iris", "299557439792", "report_iris.rmd",              "Iris_Summary_Statistics.pdf",
+  "peng", "299558308044", "report_penguins.rmd",          "The_Penguin_Report.pdf",
+  "part", "299555190551", "report_participant_count.rmd", "Participant_Count_Report.pdf"
+)
+
+# Global Decorators ------------------------------------------------------------
+#* @apiTitle Return of Information
+#* @apiDescription Plumber API for generating and uploading reports.
+
+# Endpoint A -------------------------------------------------------------------
+#* Heartbeat endpoint for testing
+#* @get /heartbeat
+function() {
+  message("API is alive!")
+}
+
+# Endpoint B -------------------------------------------------------------------
+#* Generate report and upload to GCS
+#* @param report_code:string The code of the report as specified in the config
+#* @get /render_rmd_report
+function(report_code) {
+  
+  # Get the configuration associated with the report code
+  cfg <- report_config %>% filter(code == report_code)
+  
+  # Generate a unique PDF filename
+  date_stamp <- format(Sys.time(), "%m_%d_%Y")
+  base_name  <- tools::file_path_sans_ext(cfg$pdf_name)
+  pdf_name   <- glue::glue("{base_name}_{date_stamp}_boxfolder_{cfg$box_folder_id}.pdf")
+  
+  # Render the RMarkdown file
+  rmarkdown::render(cfg$rmd_name, output_format="pdf_document", output_file=pdf_name)
+  
+  # Authenticate with Google Cloud Storage
+  scope <- c("https://www.googleapis.com/auth/cloud-platform")
+  token <- gargle::token_fetch(scopes = scope)
+  googleCloudStorageR::gcs_auth(token = token)
+  
+  # Upload .csv and .pdf files to GCS
+  filelist <- list.files(pattern = "\\.(csv|pdf)$")
+  lapply(filelist, function(x) {
+    googleCloudStorageR::gcs_upload(x, bucket = bucket, name = x)
+    print(glue::glue("Uploaded file: {x}"))
+  })
+  
+  print(glue::glue("All done. Check {bucket} for {pdf_name}"))
+
+}
